0fd4b084c575b68690700408785a488e75fcd9cc
Kevin <juwon8891@gmail.com>
2022-12-01 17:03:34 +0900

    Controller, Repository, Service

---
+++ src/boards/board.controller.ts
@@ -0,0 +1,21 @@
+import { Body, Controller, Get, Post } from '@nestjs/common';
+import { Board } from './board.model';
+import { BoardsService } from './board.service';
+
+@Controller('boards')
+export class BoardsController {
+    constructor(private boardsService: BoardsService){}
+
+    @Get('/')
+    getAllBoards(): Board[]{
+        return this.boardsService.getBoard();
+    }
+    
+    @Post()
+    createBoard(
+        @Body('title') title:string,
+        @Body('description') description:string
+    ): Board{
+        return this.boardsService.createBoard(title, description);
+    }
+}
---
+++ src/boards/board.module.ts
@@ -0,0 +1,8 @@
+import { Module } from '@nestjs/common';
+import { BoardsController } from './board.controller';
+import { BoardsService } from './board.service';
+@Module({
+  controllers: [BoardsController],
+  providers: [BoardsService]
+})
+export class BoardsModule {}
